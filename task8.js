ce2d074ae2af391dd12ce31d09c8c7ca66e0b806
nowxel <58902795+nowxel@users.noreply.github.com>
2020-05-15 15:49:52 +0300

    niemiez

---
+++ task8.js
@@ -0,0 +1,97 @@
+
+// </body>
+// 1.SELECT * 
+// FROM Employees
+// WHERE Employees.EmployeeID = 8;
+// 2. SELECT * 
+// FROM [Customers]
+// WHERE Country IN ('Sweden', 'Mexico', 'UK')
+// GROUP BY CustomerID
+// 3. SELECT CustomerName, ContactName
+// FROM Customers
+// WHERE City='London';
+// 4. SELECT COUNT(*)
+// FROM Customers
+// WHERE City='London';
+// 5. SELECT CustomerName, ContactName
+// FROM Customers
+// WHERE CustomerName LIKE 'A%' AND ContactName LIKE 'A%';
+// 6. SELECT FirstName, LastName
+// FROM Employees
+// WHERE BirthDate = (
+// 	SELECT MIN(BirthDate)
+//     From EMployees);
+// 7. SELECT TOP 3 FirstName, LastName, FLOOR(DATEDIFF(DAY, BirthDate, GetDate()) / 365.25)[Age]
+// FROM Employees
+// ORDER BY BirthDate ASC
+// 8. SELECT FirstName, LastName, BirthDate
+// FROM Employees
+// WHERE MONTH(BirthDate) = MONTH(CURDATE());
+// 9. SELECT SupplierName
+// FROM Products
+// Join Suppliers ON Products.SupplierID=Suppliers.SupplierID 
+// WHERE PRICE < 20
+// 10. UPDATE Suppliers
+// SET City='London'
+// WHERE City='Londona'
+// -------
+// SELECT COUNT(Unit)
+// FROM Products
+// Join Suppliers ON Products.SupplierID=Suppliers.SupplierID 
+// WHERE City = 'London'
+
+
+
+// thee steuetj do svietau trochqi boqom,
+// jag by sqasal, ce peredymneuw
+// nema. Tam, die projodet kater.
+// Det, 
+// mogentj producti u waden dla
+// belisni, y goqei, zel terugcomen -->
+
+// Niemiec Franceska Bahucheviza
+
+// Nia lieblu jag miesta (pa russieisku - horad)
+//  - Nadto tam ciäsnova i vieleki smoräd,
+// A panow hyich maku dy sierad harodu,
+// A prozh taho prierva rosnaho narodu!
+// Naſh brat hyich uwoidzie, - sam siabie beutzo:
+// Ci iste bias ſhapqi ci hdsie paklanitza?
+// Dyk nadtage i stydna kad nia pamylitza:
+// Paklanitza niemzu, ci hyichomu gydu!
+// Niechei jaho stazhe - nabiaretjsa stydu;
+// Niechei lepjay s domu jag teu dien nia wydu,
+// A posnat ge troudena hyich gyda, hyich niemza,
+// Hyich pana hyichoha, ci tam zhugasemzo.
+// A ce gyd da niemiez - dieti edneuy matqi:
+// Y mova padobna y edne uchwatqi, - 
+// Y abydwa lasy da zhugeuy prazi
+// Y mussit abydwa essti s krauju mazi!
+// Abudwa abdurat, abdsiarut, hyich lipqu
+// Adbiarut y ſoſhqu bitu skrypqu.
+// Pan naſh niabochciq(pamjat jamu svieta)
+// Hyich umior ci kvarta wiosna to, ci treta.
+// Hyich jeho sqrutili, zhysta abadrali,
+// Hledsit  - budsie kiepska, ce tut rabit dalei
+//  - Usio pustił arendaj; neipierst pustił gyda
+
+
+// Sdaietza b niezoha? cali kto nia wiedaje.
+// Ale pazhakeoſhy dyk wyſhla comedie. - 
+// Gyd kupił u miestie sabie kamianizu:
+// Hledsim y nasz ryski kinułs sa hranizu:
+// Pakinuł y janqu, adroksia y dieti;
+// Siauiłsa pan nuewy (heta uzo tretia)...
+// A hyichib-to pan teu? Nu ci kto pavera?
+// Hetyge samy niemiez! kab jeho cholera!
+// Ot ciapier xhadai-ty - kto kim cali stanie?
+// Ob panu ni vesti, ni slychu, nie swania,
+// Gyd panam xrabiłsia, niemiez isti bolszy
+// A mugik buł holy, ciapier isti holszy!
+// Taq y na harodsie: labiada, krapiva, - 
+// Tylqi wyrwi repu, - dyk sarastut gyva,
+// Taq, hyich cert nasieje! Otoge taq y liedsie:
+// Tutejszy sahinie, dyk worah prybudsie! 
+
+// Vstup
+// Ruthene formujut drauhu neibülſhu hälku
